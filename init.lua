7778fd988b9bdccede58dac7204f39cf8e61cb1a
karlexceed <alex.decker@gmail.com>
2020-01-09 02:08:15 -0600

    Add support for the mana mod.

--- init.lua
+++ init.lua
@@ -2,6 +2,24 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
+-- Mana Support
+local mod_mana = minetest.get_modpath("mana") ~= nil
+local cost_per_use = 200
+
+local function mana_check(player)
+	local allowed
+	if mod_mana then
+		if mana.subtract(player:get_player_name(), cost_per_use) then
+			allowed = true
+		else
+			allowed = false
+		end
+	else
+		allowed = true
+	end
+	return allowed
+end
+
 minetest.register_alias("castle:orb_day", "orbs_of_time:orb_day")
 minetest.register_alias("castle:orb_night", "orbs_of_time:orb_night")
 
@@ -13,15 +31,16 @@ minetest.register_tool("orbs_of_time:orb_day", {
 	inventory_image = "orbs_orb_day.png",
 	wield_image = "orbs_orb_day_weild.png",
 	stack_max=1,
-	groups = { tool=1 },
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
-		minetest.set_timeofday(0.5)
-		minetest.sound_play("orbs_birds", {pos=user:getpos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
+		if mana_check(user) then
+			minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
+			minetest.set_timeofday(0.5)
+			minetest.sound_play("orbs_birds", {pos=user:getpos(), loop=false})
+			if not minetest.setting_getbool("creative_mode") then
+				itemstack:add_wear(65535/8)
+			end
+			return itemstack
 		end
-		return itemstack
 	end,
 })
 
@@ -33,15 +52,16 @@ minetest.register_tool("orbs_of_time:orb_night",{
 	inventory_image = "orbs_orb_night.png",
 	wield_image = "orbs_orb_night_weild.png",
 	stack_max=1,
-	groups = { tool=1 },
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
-		minetest.set_timeofday(0)
-		minetest.sound_play("orbs_owl", {pos=user:getpos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
+		if mana_check(user) then
+			minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
+			minetest.set_timeofday(0)
+			minetest.sound_play("orbs_owl", {pos=user:getpos(), loop=false})
+			if not minetest.setting_getbool("creative_mode") then
+				itemstack:add_wear(65535/8)
+			end
+			return itemstack
 		end
-		return itemstack
 	end,
 })
 
@@ -55,13 +75,15 @@ minetest.register_tool("orbs_of_time:orb_dawn", {
 	wield_image = "orbs_orb_day_weild.png^[lowpart:75:orbs_orb_night_weild.png",
 	stack_max=1,
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
-		minetest.set_timeofday(0.2)
-		minetest.sound_play("orbs_birds", {pos=user:getpos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
+		if mana_check(user) then
+			minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
+			minetest.set_timeofday(0.2)
+			minetest.sound_play("orbs_birds", {pos=user:getpos(), loop=false})
+			if not minetest.setting_getbool("creative_mode") then
+				itemstack:add_wear(65535/8)
+			end
+			return itemstack
 		end
-		return itemstack
 	end,
 })
 
@@ -74,13 +96,15 @@ minetest.register_tool("orbs_of_time:orb_dusk",{
 	wield_image = "orbs_orb_night_weild.png^[lowpart:75:orbs_orb_day_weild.png",
 	stack_max=1,
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
-		minetest.set_timeofday(0.8)
-		minetest.sound_play("orbs_owl", {pos=user:getpos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
+		if mana_check(user) then
+			minetest.sound_play("orbs_ding", {pos=user:getpos(), loop=false})
+			minetest.set_timeofday(0.8)
+			minetest.sound_play("orbs_owl", {pos=user:getpos(), loop=false})
+			if not minetest.setting_getbool("creative_mode") then
+				itemstack:add_wear(65535/8)
+			end
+			return itemstack
 		end
-		return itemstack
 	end,
 })
 
@@ -88,58 +112,38 @@ minetest.register_tool("orbs_of_time:orb_dusk",{
 --Crafting
 -----------
 
-if minetest.get_modpath("mcl_core") then
-	minetest.register_craft({
-		output = "orbs_of_time:orb_day",
-		recipe = {
-			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
-			{"mcl_core:diamond", "mesecons:redstone","mcl_core:diamond"},
-			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
-		 },
-	})
-
-	minetest.register_craft({
-		output = "orbs_of_time:orb_night",
-		recipe = {
-			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
-			{"mcl_core:diamond", "dye:black","mcl_core:diamond"},
-			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
-		},
-	})
-else
-	minetest.register_craft({
-		output = "orbs_of_time:orb_day",
-		recipe = {
-			{"default:diamond", "default:diamond","default:diamond"},
-			{"default:diamond", "default:mese_crystal_fragment","default:diamond"},
-			{"default:diamond", "default:diamond","default:diamond"}
-		},
-	})
-
-	minetest.register_craft({
-		output = "orbs_of_time:orb_night",
-		recipe = {
-			{"default:diamond", "default:diamond","default:diamond"},
-			{"default:diamond", "default:obsidian_shard","default:diamond"},
-			{"default:diamond", "default:diamond","default:diamond"}
-		},
-	})
-end
-
-minetest.register_craft({
-	output = "orbs_of_time:orb_dawn 2",
-	recipe = {
-		{"orbs_of_time:orb_day"},
-		{"orbs_of_time:orb_night"},
-	},
+minetest.register_craft( {
+  output = "orbs_of_time:orb_day",
+  recipe = {
+    {"default:diamond", "default:diamond","default:diamond"},
+    {"default:diamond", "default:mese_crystal","default:diamond"},
+    {"default:diamond", "default:diamond","default:diamond"}
+   },
 })
 
 minetest.register_craft({
-	output = "orbs_of_time:orb_dusk 2",
-	recipe = {
-		{"orbs_of_time:orb_night"},
-		{"orbs_of_time:orb_day"},
-	},
+  output = "orbs_of_time:orb_night",
+  recipe = {
+   {"default:diamond", "default:diamond","default:diamond"},
+   {"default:diamond", "default:obsidian_shard","default:diamond"},
+   {"default:diamond", "default:diamond","default:diamond"}
+  },
+})
+
+minetest.register_craft({
+  output = "orbs_of_time:orb_dawn 2",
+  recipe = {
+   {"orbs_of_time:orb_day"},
+   {"orbs_of_time:orb_night"},
+  },
+})
+
+minetest.register_craft({
+  output = "orbs_of_time:orb_dusk 2",
+  recipe = {
+   {"orbs_of_time:orb_night"},
+   {"orbs_of_time:orb_day"},
+  },
 })
 
 -----------
